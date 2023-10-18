16dd329abf332023a8cac8c7dc8707969fde8824
LeoAnanya <leo.ananya10@gmail.com>
2023-10-18 21:23:41 +0530

    3rd Party Payment API integration using PayPal

---
+++ src/main/java/com/rdPartyPaymentAPIIntegration/paymentAPIIntegration/PaymentApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.rdPartyPaymentAPIIntegration.paymentAPIIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdPartyPaymentAPIIntegration/paymentAPIIntegration/PaymentApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdPartyPaymentAPIIntegration.paymentAPIIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
